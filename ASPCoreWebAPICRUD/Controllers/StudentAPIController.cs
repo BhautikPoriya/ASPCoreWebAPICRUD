93bbb682180422731e8ff324e05a6a8cf1ef53d9
BhautikPoriya <bporiya2001@gmail.com>
2023-09-11 17:55:11 +0530

    Add project files.

---
+++ ASPCoreWebAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,49 @@
+﻿using ASPCoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+namespace ASPCoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly CodeFirstDBContext codeFirst;
+
+        public StudentAPIController(CodeFirstDBContext codeFirst) 
+        {
+            this.codeFirst = codeFirst;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await codeFirst.Students.ToListAsync();
+            return Ok(data);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentsbyID(int id)
+        {
+            var data = await codeFirst.Students.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return data; 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await codeFirst.Students.AddAsync(std);
+            await codeFirst.SaveChangesAsync();
+            return Ok(std);
+        }
+
+
+    }
+}
---
+++ ASPCoreWebAPICRUD/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASPCoreWebAPICRUD.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; } = null!;
+        public string StudentGender { get; set; } = null!;
+        public int StudentAge { get; set; }
+        public int StudentStandard { get; set; }
+    }
+}
